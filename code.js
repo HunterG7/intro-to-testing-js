01a34be2d7057744360b6146b5854e66276d46f1
Hunter Grubbs <hunter.grubbs07@gmail.com>
2023-02-23 11:20:43 -0600

    isVoewl tests and solutions

--- code.js
+++ code.js
@@ -14,3 +14,23 @@ function sayHello(input){
 function isFive(input){
     return (input === 5);
 }
+
+function isEven(input){
+    input = parseFloat(input);
+    if (typeof input === 'number' && isNaN(input) === false) {
+        return (input % 2 === 0);
+    } else {}
+    return false;
+}
+
+function isVowel(input){
+    input = input.toLowerCase();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    vowels.forEach(function(vowel) {
+        if (input === vowel) {
+            return true;
+        }
+    });
+    return false;
+}
+console.log(isVowel('A'));
--- tests.js
+++ tests.js
@@ -49,3 +49,70 @@ describe('isFive', function () {
         expect(isFive('5')).toBe(false);
     });
 });
+
+
+describe('isEven', function() {
+    it('should always return boolean', function () {
+        expect(typeof isEven()).toBe('boolean');
+    });
+    it('returns true when executed with isEven(2)', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('returns true when executed with isEven(-4)', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('returns false when executed with isEven(3)', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('returns false when executed with isEven("banana")', function () {
+        expect(isEven('banana')).toBe(false);
+    });
+    it('returns true when executed with isEven("8")', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('returns false when executed with isEven(INFINITY)', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('return false when called with a boolean input', function () {
+       expect(isEven(true) || isEven(false)).toBe(false );
+    });
+    it('returns false when called without an argument', function () {
+        expect(isEven()).toBe(false);
+    });
+});
+
+// isVowel always returns a boolean
+// isVowel("a") returns true
+// isVowel("A") returns true
+// isVowel("y") returns false
+// isVowel(4) returns false
+// isVowel(true) or isVowel(false) both return false
+// isVowel("banana") returns false
+// isVowel() returns false
+
+describe('isVowel', function () {
+   it('isVowel always returns a boolean', function () {
+       expect(typeof isVowel()).toBe('boolean');
+   });
+   it('isVowel("a") returns true', function () {
+       expect(isVowel('a')).toBe(true);
+   });
+    it('isVowel("A") returns true', function () {
+        expect(isVowel('A')).toBe(true);
+    });
+    it('isVowel("y") returns true', function () {
+        expect(isVowel('y')).toBe(true);
+    });
+    it('isVowel(4) returns true', function () {
+        expect(isVowel(4)).toBe(true);
+    });
+    it('isVowel(true) or isVowel(false) both return false', function () {
+        expect(isVowel(true) || isVowel(false)).toBe(false);
+    });
+    it('isVowel("banana") returns false', function () {
+       expect(isVowel('banana')).toBe(false);
+    });
+    it('isVowel() returns false', function () {
+        expect(isVowel()).toBe(false);
+    });
+});
